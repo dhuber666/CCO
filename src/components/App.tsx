32f260d018937b87202d40c77665cc7fdbb7842d
dhuber666 <dhuber666@gmail.com>
2020-02-25 00:11:30 +0100

    tried a few things to make a logo with dynamic color

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HomeScreen from "../screens/HomeScreen";
 import MembersScreen from "../screens/MembersScreen";
+import ClubScreen from "../screens/ClubScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
         </Route>
         <Route exact path="/members">
           <MembersScreen />
+        </Route>
+        <Route exact path="/Club">
+          <ClubScreen />
         </Route>
       </Switch>
     </Router>
--- src/screens/ClubScreen.tsx
+++ src/screens/ClubScreen.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { ReactComponent as Logo } from "../assets/logo/logo.svg";
+import { ReactSVG } from "react-svg";
 
 
 const ClubScreen = () => {
-    return <p>Club</p>
+    return (
+
+        <ReactSVG src="logo.svg" beforeInjection={svg => {
+            svg.setAttribute("style", "background: orange")
+            svg.setAttribute("style", "width: 300px")
+        }} />
+
+    )
+
 }
+
+{/* <Logo height="300" width="300">
+            <use xlinkHref="CCO_Club" fill="orange" />
+        </Logo> */}
 
 
 export default ClubScreen;
